caceb42c7756b22ec5dca99d6b73df7af8bc192c
Gonçalo Vieira <vieiragoncalo23@gmail.com>
2022-04-19 17:32:40 +0100

    finalized product page style

--- src/client/components/Cars/index.tsx
+++ src/client/components/Cars/index.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import vehicles from "../../../vehicles.json";
+import Pagination from "../../utils/Pagination/index";
 
-const House = () => {
+const Cars = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(6);
+  const navigate = useNavigate();
 
-    return (
-        <>
-            <h1>This is Cars page!</h1>
-        </>
-    )
+  
+
+  //GET CURRENT POSTS
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = Object.entries(vehicles).slice(
+    indexOfFirstPost,
+    indexOfLastPost
+  );
+
+  return (
+    <>
+      <h1>This is Cars page!</h1>
+    </>
+  );
 };
 
-
-export default House;
+export default Cars;
